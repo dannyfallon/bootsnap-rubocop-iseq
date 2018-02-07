85165025037679c02841960b67f1b1fbff63af1d
Danny Fallon <danny@intercom.io>
2018-02-07 13:36:25 +0000

    Swap to full config for Bootsnap
    Also change to using `/tmp/cache` for the cache because we don't
    want to write to `tmp/cache` when it lives out in a container's
    host filesystem; that's just bad news.

--- config/boot.rb
+++ config/boot.rb
@@ -2,4 +2,13 @@ ENV['BUNDLE_GEMFILE'] ||= File.expand_path('../Gemfile', __dir__)
 
 require 'bundler/setup' # Set up gems listed in the Gemfile.
 require 'bootsnap'
-require 'bootsnap/setup'
+env = ENV["RAILS_ENV"] || "development"
+Bootsnap.setup(
+  cache_dir:            "/tmp/cache",
+  development_mode:     env == "development",
+  load_path_cache:      true,
+  autoload_paths_cache: true,
+  disable_trace:        true,
+  compile_cache_yaml:   true,
+  compile_cache_iseq:   true,
+)
